c771872c287cc5940b29368091eb16dc093c0575
AhsanKhan <ahsankhan609@gmail.com>
2026-06-28 13:01:34 +0500

    Add a new script 'bestdayever_v3.sh' that delivers motivational messages to the user based on input names

---
+++ scripts/bestdayever_v3.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# name=$1 # just capture first argument
+name="$*" # capture all arguments such as ./script.sh john smith
+
+echo "Good morning, $name!  Today is going to be the best day ever!"
+
+sleep 2
+
+echo "Hey $name, Remember to smile and stay positive throughout the day."
+
+sleep 2
+
+echo "Take a moment to appreciate the little things in life."
+
+sleep 2
+
+echo "$name, you are capable of achieving great things today!"
+
+sleep 2
+
+echo "Ok $name, let's get started and make today amazing!"
