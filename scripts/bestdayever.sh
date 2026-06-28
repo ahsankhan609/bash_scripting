278365203de8e40386b54b11bfddcf5c836374a6
artdesigner609@gmail.com <your.email@example.com>
2026-06-28 02:49:05 -0400

    update script for using variables in it

--- scripts/bestdayever.sh
+++ scripts/bestdayever.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/zsh
 
-echo "Good morning! Today is going to be the best day ever!"
+name="John"
+
+echo "Good morning, $name!  Today is going to be the best day ever!"
 
 sleep 2
 
-echo "Remember to smile and stay positive throughout the day."
+echo "Hey $name, Remember to smile and stay positive throughout the day."
 
 sleep 2
 
@@ -12,8 +14,8 @@ echo "Take a moment to appreciate the little things in life."
 
 sleep 2
 
-echo "You are capable of achieving great things today!"
+echo "$name, you are capable of achieving great things today!"
 
 sleep 2
 
-echo "Ok, let's get started and make today amazing!"
+echo "Ok $name, let's get started and make today amazing!"
